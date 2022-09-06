71e558c82e91549fc43c85fbe64a5acc461b3f30
Konstantin <love_road@mail.ru>
2022-09-06 22:06:11 +0300

    Home_1

--- Home_From_1_Seminar/Z007.py
+++ Home_From_1_Seminar/Z007.py
@@ -7,15 +7,3 @@ for x in 1, 0:
         for z in 1, 0:
             print(f"{x = } {y = } {z = }  {not(x or y or z) == (not x and not y and not z)}") 
            
-                      
-
-
-    
-
-
-
-
-
-            
-
-
